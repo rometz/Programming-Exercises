4d0695d4ea40fac42d488a9c0bb3e69eb9b983cd
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-02 17:18:39 -0500

    Finish ch3 ex 1 Minimum

---
+++ eloquentJavascriptV3/ch3ExMinimum.js
@@ -0,0 +1,9 @@
+function minimum(valOne, valTwo) {
+    if (valOne < valTwo) {
+        console.log(valOne);
+    } else if (valTwo < valOne) {
+        console.log(valTwo);
+    } else {
+        console.log('Values are equal.');
+    }
+}
