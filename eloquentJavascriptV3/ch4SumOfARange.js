def8abc598d5fbf29ce1980e375d5f6b4ec95887
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-14 21:49:35 -0500

    Add solution for Sum of a Range

---
+++ eloquentJavascriptV3/ch4SumOfARange.js
@@ -0,0 +1,12 @@
+let rangeArray = []
+
+function range(start, end, step) {
+    for (i = start; i <= end; i += step) {
+        rangeArray.push(i);
+    }
+    return rangeArray
+}
+
+function sum(array) {
+    return array.reduce((a,b) => a + b, 0);
+}
